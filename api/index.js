22550271fd65bf1710d9b7cf4cde0b2ec48a1dff
Yuri Cruz <yuricruzf@gmail.com>
2024-05-01 20:54:47 -0300

    Using authRoter, applying bodyParse for tests

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import userRouter from "./routes/user.js";
+import authRouter from "./routes/auth.js";
+
+import bodyParser from "body-parser";
 
 const app = express();
 
+//Fazendo o app entender o formato json das respostas vindas do controller
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use("/api/users/", userRouter);
+app.use("/api/auth/", authRouter);
 
 app.listen(8001, () => {
   console.log("Servidor rodando para a porta 8001");
