cb704964142b5984e2caefb885b9e08c79b87225
Yuri Cruz <yuricruzf@gmail.com>
2024-05-02 00:15:19 -0300

    Controller of login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,8 @@
 import { db } from "../connect.js";
 import bcrypt from "bcrypt";
 
+import jwt from 'jsonwebtoken'
+
 export const register = (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
 
@@ -67,4 +69,53 @@ export const register = (req, res) => {
   );
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  const { email, password } = req.body;
+
+  db.query(
+    "SELECT * FROM user WHERE email = ?",
+    [email],
+    async (error, data) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          msg: "Aconteceu algum problema no servidor. Tente novamente mais tarde",
+        });
+      }
+      if (data.length == 0) {
+        return res.status(404).json({
+          msg: "Usuário não encontrado!",
+        });
+      } else {
+        const user = data[0]; //Já que devolve um array, preciso acessar a posição dele
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword) {
+          return res.status(422).json({ msg: "Senha incorreta" });
+        }
+
+        try {
+          //Fazendo a validação para ver se o usuário está logado
+          const refreshToken = jwt.sign({
+            exp: Math.floor(Date.now()/1000) + 24 * 60 * 60,
+            id: user.password
+          },
+          process.env.REFRESH,
+          {algorithm: "HS256"}
+          )
+          const token = jwt.sign({
+            exp: Math.floor(Date.now()/1000) + 3600,
+            id: user.password
+          },
+          process.env.TOKEN,
+          {algorithm: "HS256"}
+          )
+          res.status(200).json({msg: "Usuário logado com sucesso.", token, refreshToken})
+        } catch(err) {
+          console.log(err);
+          return res.status(500).json({msg: "Aconteceu algum problema no servidor. Tente novamente mais tarde"})
+        }
+      }
+    }
+  );
+};
