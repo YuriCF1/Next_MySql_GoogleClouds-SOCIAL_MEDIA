872854544ed268b2059a21f8659df26034a65355
Yuri Cruz <yuricruzf@gmail.com>
2024-05-04 23:49:20 -0300

    Changing responsive variables

--- client/social-media/tailwind.config.ts
+++ client/social-media/tailwind.config.ts
@@ -7,14 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': {'max': '500px'},
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        // "cover-rede": "url('./public/images/fundo1.jpg')",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
