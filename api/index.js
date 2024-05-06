4753f110a766e4aa189d146363b256d6d2919c96
Yuri Cruz <yuricruzf@gmail.com>
2024-05-05 22:20:10 -0300

    Adding domains to cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
+import cors from "cors";
 
 import bodyParser from "body-parser";
 
@@ -9,7 +10,11 @@ const app = express();
 //Fazendo o app entender o formato json das respostas vindas do controller
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({extended: false}))
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(cors({ credentials: true, origin: allowedOrigins })); //crediantals: true = o servidor pode enviar e receber cookies e cabeçalhos de autenticação.
+
+app.use(bodyParser.urlencoded({ extended: false })); //Recebendo os dados pelo método x-www-form-urlencoded do postman
 
 app.use("/api/users/", userRouter);
 app.use("/api/auth/", authRouter);
