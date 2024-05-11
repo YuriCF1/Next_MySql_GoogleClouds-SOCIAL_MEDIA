345b38e5be63bde3ccc5c09d4df4ecf72d2b6a2a
Yuri Cruz <yuricruzf@gmail.com>
2024-05-11 03:39:12 -0300

    Adding feed and sidebar

--- client/social-media/src/app/page.tsx
+++ client/social-media/src/app/page.tsx
@@ -1,9 +1,11 @@
-'use client+'
+'use client'
 
 import Image from "next/image";
 import Header from "./components/Header";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import SideBar from "./components/SideBar";
+import Feed from "./components/Feed";
 
 export default function Home() {
   const router = useRouter()
@@ -16,8 +18,12 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-100">
+    <main className="flex flex-col items-center justify-between bg-white">
       <Header />
+      <div className="w-full flex justify-start pt-10">
+        <SideBar />
+        <Feed />
+      </div>
     </main>
   );
 }
