6c59c70e07a7d1aa8a6d6e26d1155b452b327297
Yuri Cruz <yuricruzf@gmail.com>
2024-05-14 22:08:03 -0300

    Updating post component to manage data from backend

--- client/social-media/src/app/components/Post.tsx
+++ client/social-media/src/app/components/Post.tsx
@@ -7,6 +7,7 @@ import { IPost } from '../interfaces/IPost'
 
 const Post = (props: { post: IPost }) => {
     const [user, setUser] = useState<IUser | undefined>(undefined)
+    const { post_desc, img, username, userImg, created_at } = props.post
 
     useEffect(() => {
         let value = localStorage.getItem('rede-social:user');
@@ -15,14 +16,16 @@ const Post = (props: { post: IPost }) => {
         }
     }, [])
 
-    const { post_desc, img, username, userImg } = props.post
+    let date = new Date(created_at)
+    let formatedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
     return (
         <div className='w-1/3 bg-zinc-100 rounded-lg p-4 shadow-2xl m-1'>
             <header className='flex gap-2 pb-4 border-b items-center'>
                 <img className="w-10 h-10 rounded-full" src={userImg ? userImg : 'https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1'} alt="Imagem do usuário autor do poste" />
                 <div className='flex flex-col'>
                     <span className='font-semibold'>{username}</span>
-                    <span className='text-xs'>26/12/2021</span>
+                    <span className='text-xs'>{formatedDate}</span>
                 </div>
             </header>
             {post_desc && (
