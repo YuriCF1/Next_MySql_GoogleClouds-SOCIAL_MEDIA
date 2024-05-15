f9b683984cdbb476d52fd21aff2d5131341f33f4
Yuri Cruz <yuricruzf@gmail.com>
2024-05-14 22:08:22 -0300

    Getting data from backend and giving it to post component

--- client/social-media/src/app/components/Feed.tsx
+++ client/social-media/src/app/components/Feed.tsx
@@ -1,23 +1,23 @@
+import { useEffect, useState } from "react"
 import Post from "./Post"
+import { IPost } from "../interfaces/IPost";
+import { makeRequest } from "../../../axios";
 
 const Feed = () => {
-    const posts = [{
-        id: 1,
-        post_desc: 'teste',
-        img: 'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        username: 'user',
-        userImg: ''
-    }, {
-        id: 2,
-        post_desc: 'teste',
-        img: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        username: 'user',
-        userImg: ''
-    }]
+
+    const [posts, setPosts] = useState<IPost[] | undefined>(undefined);
+
+    useEffect(() => {
+        makeRequest.get("/post/").then((res) => {
+            setPosts(res.data.data)
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, [])
 
     return (
-        <div className="flex flex-col items-center ">
-            {posts.map((post, id) => {
+        <div className="flex flex-col items-center gap-5 w-full">
+            {posts?.map((post, id) => {
                 return (
                     <Post post={post} key={id} />
                 )
