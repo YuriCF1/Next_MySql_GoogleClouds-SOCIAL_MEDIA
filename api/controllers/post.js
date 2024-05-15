3d3ab1013aeb4a6889d88191379160cd8530a33c
Yuri Cruz <yuricruzf@gmail.com>
2024-05-14 22:07:04 -0300

    Setting getPost route

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -6,10 +6,10 @@ export const createPost = (req, res) => {
   if (!post_desc && !img) {
     return res
       .status(422)
-      .json({ msg: "O post precisa ter umt exto ou uma imagem!" });
+      .json({ msg: "O post precisa ter um texto ou uma imagem!" });
   }
 
-  db.query("INSERT INTO post SET ?", { desc, img, userId }, (error) => {
+  db.query("INSERT INTO posts SET ?", { post_desc, img, userId }, (error) => {
     if (error) {
       console.log(error);
       return res.status(500).json({
@@ -21,4 +21,18 @@ export const createPost = (req, res) => {
   });
 };
 
-export const getPost = () => {};
+export const getPost = (req, res) => {
+  db.query(
+    "SELECT p.*, u.username, userImg FROM posts AS p JOIN user AS u ON (u.id = p.userId)",
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          msg: "Aconteceu algum erro no servidor, tente novamente mais tarde.",
+        });
+      } else if (data) {
+        return res.status(200).json({ data });
+      }
+    }
+  );
+};
