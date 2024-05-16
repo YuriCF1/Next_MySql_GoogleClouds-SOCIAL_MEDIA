f6f54cb20f052b59705850437c09893509562c30
Yuri Cruz <yuricruzf@gmail.com>
2024-05-16 14:42:41 -0300

    Using cookie parser and setting cors option

--- api/index.js
+++ api/index.js
@@ -4,18 +4,34 @@ import authRouter from "./routes/auth.js";
 import postRouter from "./routes/post.js";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //Fazendo o app entender o formato json das respostas vindas do controller
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:3000"];
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //Esta opção indica se a requisição pode incluir cookies, autorizações HTTP ou certificados TLS. Quando configurada como true, o servidor aceita e envia cookies com as requisições.
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Credentials",
+  ],
+};
 
-app.use(cors({ credentials: true, origin: allowedOrigins })); //crediantals: true = o servidor pode enviar e receber cookies e cabeçalhos de autenticação.
+/*Esta opção define quais cabeçalhos HTTP podem ser usados quando a solicitação é feita. No caso, os cabeçalhos permitidos são:
 
+Content-Type: Define o tipo de mídia do recurso.
+Authorization: Usado para enviar informações de autenticação.
+Access-Control-Allow-Credentials: Indica se a resposta pode ser exposta ao navegador e se o navegador deve enviar credenciais como cookies junto com a requisição.*/
+
+//Middleware
 app.use(bodyParser.urlencoded({ extended: false })); //Recebendo os dados pelo método x-www-form-urlencoded do postman
-
+app.use(cors(corsOptions)); //crediantals: true = o servidor pode enviar e receber cookies e cabeçalhos de autenticação.
+app.use(cookieParser());
 app.use("/api/users/", userRouter);
 app.use("/api/auth/", authRouter);
 app.use("/api/post/", postRouter);
