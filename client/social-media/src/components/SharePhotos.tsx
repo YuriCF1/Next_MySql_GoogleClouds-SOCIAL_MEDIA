75403011988be7712598125a7e42c9171f62d992
Yuri Cruz <yuricruzf@gmail.com>
2024-05-21 10:29:58 -0300

    Adding upload functiong and changing posts logic

--- client/social-media/src/components/SharePhotos.tsx
+++ client/social-media/src/components/SharePhotos.tsx
@@ -1,18 +1,21 @@
 import { UserContext } from "@/context/UserContext"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { useContext, useState } from "react"
-import { FaPaperPlane, FaRegComment, FaThumbsUp, FaUserFriends } from "react-icons/fa"
+import { useContext, useMemo, useState } from "react"
+import { FaPaperPlane, FaUserFriends } from "react-icons/fa"
 import { TbPhoto } from "react-icons/tb"
 import { makeRequest } from "../../axios"
 
 const SharePhotos = () => {
-
     const { user } = useContext(UserContext)
-
     const [post_desc, setDesc] = useState('');
-
-
     const [img, setImg] = useState<File | null>(null)
+
+
+    const imageUrl = useMemo(() => {
+        if (img) {
+            return URL.createObjectURL(img);
+        }
+    }, [img]);
 
     const queryClient = useQueryClient() //Pegando o queryClient iniciado lá no layout
     const mutation = useMutation({
@@ -21,13 +24,36 @@ const SharePhotos = () => {
                 return res.data
             })
         }, onSuccess: () => {
+            ////Após a mutação bem-sucedida, a consulta aos posts será invalidada e, quando feita novamente, buscará os dados atualizados após a criação do novo post.
             queryClient.invalidateQueries({ queryKey: ["posts"] })
         }
     })
 
+    const upload = async () => {
+        try {
+            const formData = new FormData //O  multer exige o envio da imagem por um FormData
+            img && formData.append("file", img)
+            const res = await makeRequest.post("upload/", formData)
+            return res.data
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const sharePost = async () => {
+        let imgUrl = ''
+        if (img) {
+            imgUrl = await upload() //Que na verdade retorna o nome da imagem salva na apsta public/uploads
+            console.log(imgUrl);
+        }
+        mutation.mutate({ post_desc, img: imgUrl, userId: user?.id })
+        setDesc('')
+        setImg(null)
+    }
+
     return (
         <div className="w-1/3 bg-white rounded-lg p-4 shadow-md flex flex-col gap-3">
-            {img && <img className='rounded-lg' src={URL.createObjectURL(img)} alt="Imagem do post" />}
+            {img && <img className='rounded-lg' src={imageUrl} alt="Imagem do post" />}
             <div className='flex gap-2 pt-6'>
                 <img src={user?.userImg ? user.userImg : 'https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1'}
                     alt="Imagem de pefil do usuário" className="w-12 h-12 rounded-full" />
@@ -38,7 +64,7 @@ const SharePhotos = () => {
                         className='bg-zinc-100 w-full focus-visible:outline-none border-4 rounded-full px-3 mx-5' type="text"
                         placeholder={`Compartilhe seus momentos, ${user?.username}!`} />
                     <button>
-                        <FaPaperPlane onClick={() => mutation.mutate({ post_desc, userId: user?.id })} />
+                        <FaPaperPlane onClick={() => sharePost()} />
                     </button>
                 </div>
             </div>
