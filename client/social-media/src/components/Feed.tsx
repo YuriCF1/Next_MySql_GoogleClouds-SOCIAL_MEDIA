27e598cd644be5de8d1378049f14f31935485c27
Yuri Cruz <yuricruzf@gmail.com>
2024-05-21 10:30:37 -0300

    Making adjustments to post logic

--- client/social-media/src/components/Feed.tsx
+++ client/social-media/src/components/Feed.tsx
@@ -2,26 +2,34 @@ import { useEffect, useState } from "react"
 import Post from "./Post"
 import { IPost } from "../app/interfaces/IPost";
 import { makeRequest } from "../../axios";
+import SharePhotos from "./SharePhotos";
+import { useQuery } from "@tanstack/react-query";
 
 const Feed = () => {
 
-    const [posts, setPosts] = useState<IPost[] | undefined>(undefined);
+    const { data, isLoading, error } = useQuery<IPost[] | undefined>({
+        queryKey: ["posts"],
+        queryFn: () =>
+            makeRequest.get("/post/").then((res) => {
+                return res.data.data
+            })
+    })
 
-    useEffect(() => {
-        makeRequest.get("/post/").then((res) => {
-            setPosts(res.data.data)
-        }).catch((err) => {
-            console.log(err);
-        })
-    }, [])
+    if (error) {
+        console.log(error);
+    }
 
     return (
         <div className="flex flex-col items-center gap-5 w-full">
-            {posts?.map((post, id) => {
-                return (
-                    <Post post={post} key={id} />
-                )
-            })}
+            <SharePhotos />
+            {isLoading ? <span>Carregando...</span> :
+                <div className="w-full flex flex-col-reverse gap-5 items-center"> {data?.map((post, id) => {
+                    return (
+                        <Post post={post} key={id} />
+                    )
+                })}
+                </div>
+            }
         </div>
     )
 }
