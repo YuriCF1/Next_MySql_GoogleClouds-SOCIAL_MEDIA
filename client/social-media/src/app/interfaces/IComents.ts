7f9a661d04136e564887626f577260fb2395c686
Yuri Cruz <yuricruzf@gmail.com>
2024-05-24 22:46:27 -0300

    Creating interface of comments

---
+++ client/social-media/src/app/interfaces/IComents.ts
@@ -0,0 +1,9 @@
+export default interface IComments {
+    id: number,
+    comment_deesc: string,
+    user_img: string,
+    comment_user_id: number,
+    username: string,
+    post_id: number,
+    created_at: string
+}
