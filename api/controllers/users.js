98d555570d3cda1fcc83d29a40bef2d83749c5fe
Yuri Cruz <yuricruzf@gmail.com>
2024-05-28 02:25:39 -0300

    Getting user data

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,5 +1,23 @@
+import { db } from "../connect.js";
+
 export const getUser = (req, res) => {
-  res.status(200).json({
-    msg: "Funcionando Controller",
-  });
+  const id = req.query.id;
+
+  if (!id) {
+    res.status(422).json({ msg: "O id do usuário é necessário" });
+  }
+
+  db.query(
+    "SELECT username, userImg, bgImg FROM user WHERE id = ?",
+    [id],
+    (error, data) => {
+      if (error) {
+        res.status(500).json({
+          msg: "Ops, aconteceu um erro no servidor. Tente novamente mais tarde.",
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    }
+  );
 };
