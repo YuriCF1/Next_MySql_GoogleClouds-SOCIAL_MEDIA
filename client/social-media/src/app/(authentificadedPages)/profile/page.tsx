e1c31181a2492b4731802619369ec950e8239d91
Yuri Cruz <yuricruzf@gmail.com>
2024-06-10 19:33:35 -0300

    Creating fallowing logic

--- client/social-media/src/app/(authentificadedPages)/profile/page.tsx
+++ client/social-media/src/app/(authentificadedPages)/profile/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { makeRequest } from "../../../../axios"
 import Feed from '@/components/Feed'
 import { IPost } from '@/app/interfaces/IPost'
+import { useContext, useState } from 'react'
+import { UserContext } from '@/context/UserContext'
+import IFriendship from '@/app/interfaces/IFriendship'
 
 const ProfilePage = ({ searchParams }: { searchParams: { id: string } }) => { //Altomaticamente já pega os parâmetros passados pela URL
+    const { user } = useContext(UserContext)
+    const queryClient = useQueryClient()
+    const [fallowedState, setFallowedState] = useState(false)
+
     const profileQuery = useQuery({
         queryKey: ["profile", searchParams.id], //Diferenciando cada proefile pelos id's do usuário
         queryFn: () => makeRequest.get("users/get-user?id=" + searchParams.id)
@@ -13,10 +20,6 @@ const ProfilePage = ({ searchParams }: { searchParams: { id: string } }) => { //
                 return res.data[0]
             })
     })
-
-    if (profileQuery.error) {
-        console.log("Error profile", profileQuery.error);
-    }
 
     const postQuery = useQuery<IPost[] | undefined>({
         queryKey: ["posts"],
@@ -26,9 +29,57 @@ const ProfilePage = ({ searchParams }: { searchParams: { id: string } }) => { //
             })
     })
 
+    if (profileQuery.error) {
+        console.log("Error profile", profileQuery.error);
+    }
+
     if (postQuery.error) {
         console.log(postQuery.error);
     }
+
+    const friendshipQuery = useQuery({
+        queryKey: ["friendship"], //Não precisa de id para diferenciar pois ela sempre retornará apenas de quem ta logado
+        queryFn: () => makeRequest.get('/friendship/?fallower_id=' + user?.id)
+            .then((res) => {
+                res.data.data.find((e: IFriendship) => {
+                    console.log(e);
+                    if (e.fallowed_id === +searchParams.id) { //Verificando se algum resultado terá o id do usuário, então quer dizer que ele já estará seguindo
+                        setFallowedState(true)
+                        console.log('Sigo?', fallowedState);
+                    }
+                })
+                return res.data.data
+            }),
+        enabled: !!user?.id
+    })
+
+    if (friendshipQuery.error) {
+        console.log(friendshipQuery.error);
+    }
+
+    const mutation = useMutation({
+        mutationFn: async (unfallow: { fallowedState: boolean, fallower_id: number, fallowed_id: number }) => {
+            if (fallowedState) {
+                return makeRequest //Adicionar return é essencial para o funcionamento da onSuccess
+                    .delete(`friendship/?fallower_id=${unfallow.fallower_id}&fallowed_id=${unfallow.fallowed_id}`)
+                    .then((res) => {
+                        setFallowedState(false)
+                        return res.data.data
+                    })
+            } else {
+                console.log('Tetando seguir', fallowedState, unfallow.fallowed_id);
+                setFallowedState(true)
+                return makeRequest
+                    // .post(`friendship/}`, { fallower_id: unfallow.fallower_id, fallowed_id: unfallow.fallowed_id })
+                    .post(`friendship/`, { fallower_id: user?.id, fallowed_id: searchParams.id })
+                    .then((res) => {
+                        return res.data.data
+                    })
+            }
+        }, onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['friendship'] })
+        }
+    })
 
     return (
         <div className="w-1/2 flex flex-col items-center">
@@ -39,8 +90,16 @@ const ProfilePage = ({ searchParams }: { searchParams: { id: string } }) => { //
                     <img className='w-40 h-40 rounded-full border-zinc-100 border-4' src={profileQuery.data?.userImg ? profileQuery.data.userImg : 'https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1'}
                         alt="Imagem de pefil do usuár io" />
                     <span className='text-2xl font-bold pl-2'>{profileQuery.data?.username}</span>
+                    {user?.id != +searchParams.id &&
+                        <button onClick={() => user && mutation.mutate({ fallowedState, fallowed_id: +searchParams.id, fallower_id: user?.id })}
+                            className={`w-56 py-2 font-semibold rounded-md ml-48
+                                ${fallowedState ?
+                                    'w-1/2 py-2 font-semibold rounded-md bg-zinc-400 hover:text-black'
+                                    : 'bg-green-600 text-white hover:bg-green700'}`}>{fallowedState ? 'Deixar de seguir' : 'Seguir'}</button>
+                    }
                 </div>
             </div>
+            {/* Passando um + na frente, transforma o string em número */}
             <div className='pt-36 w-full'>
                 <Feed post={postQuery.data} />
             </div>
