aaa99ba00f24c67ff05e165b4cee72101f5fb846
Yuri Cruz <yuricruzf@gmail.com>
2024-06-16 00:08:41 -0300

    Creating function and route to update users

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -21,3 +21,32 @@ export const getUser = (req, res) => {
     }
   );
 };
+
+export const updateUser = (req, res) => {
+  const { id, username, userImg, bgImg } = req.body;
+
+  if (!username || !userImg || !bgImg) {
+    return res
+      .status(422)
+      .json({ msg: "Algum dado precisa ser enviado para a atualização!" });
+  }
+
+  db.query(
+    "UPDATE user SET username = ?, userImg = ?, bgImg = ? WHERE id = ?",
+    [username, userImg, bgImg, id],
+    (error, data) => {
+      if (error) {
+        return res.status(500).json({
+          msg: "Ops, aconteceu um erro no servidor. Tente novamente mais tarde.",
+        });
+      } else if (data.affectedRows > 0) {
+        //Vai retornar as linhas afetadas
+        return res.status(200).json("Atualizado com sucesso");
+      } else {
+        return res
+          .status(404)
+          .json("Usuário não encontrado ou nenhum dado foi atualizado");
+      }
+    }
+  );
+};
