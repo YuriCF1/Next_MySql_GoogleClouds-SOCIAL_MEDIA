167341cd71bb9759b34388e1bce52a25414c29ee
Yuri Cruz <yuricruzf@gmail.com>
2024-06-21 12:23:03 -0300

    Changing validation performance

--- api/middleware/tokenValidation.js
+++ api/middleware/tokenValidation.js
@@ -20,18 +20,30 @@
 import jwt from "jsonwebtoken";
 
 export const checkToken = (req, res, next) => {
-  const authHeader = req.headers.cookie?.split("; ")[0]; //Desistruturando o token
+  // Extrair o cookie de autorização
+  const authHeader = req.headers.cookie
+    ?.split("; ")
+    .find(cookie => cookie.startsWith("accessToken="));
+
+  // Extrair o token do cookie
   const token = authHeader && authHeader.split("=")[1];
 
   if (token) {
     try {
+      // Verificar o token
       jwt.verify(token, process.env.TOKEN);
       next();
     } catch (error) {
       console.log(error);
-      res.status(400).json({ msg: "Token inválido" });
+      if (error.name === "TokenExpiredError") {
+        res.status(401).json({ msg: "Token expirado" });
+      } else if (error.name === "JsonWebTokenError") {
+        res.status(400).json({ msg: "Token inválido" });
+      } else {
+        res.status(400).json({ msg: "Erro de autenticação" });
+      }
     }
   } else {
-    return res.status(401).json({ msg: "Acesso negado" });
+    return res.status(401).json({ msg: "Acesso negado. Token não fornecido" });
   }
 };
