45f21a56147e92714d3cffbb4db80a5c5bb9504b
Yuri Cruz <yuricruzf@gmail.com>
2024-06-21 06:14:28 -0300

    Testing new acess token

--- api/middleware/tokenValidation.js
+++ api/middleware/tokenValidation.js
@@ -1,3 +1,22 @@
+// import jwt from "jsonwebtoken";
+
+// export const checkToken = (req, res, next) => {
+//   const authHeader = req.headers.cookie?.split("; ")[0]; //Desistruturando o token
+//   const token = authHeader && authHeader.split("=")[1];
+
+//   if (token) {
+//     try {
+//       jwt.verify(token, process.env.TOKEN);
+//       next();
+//     } catch (error) {
+//       console.log(error);
+//       res.status(400).json({ msg: "Token inválido" });
+//     }
+//   } else {
+//     return res.status(401).json({ msg: "Acesso negado" });
+//   }
+// };
+
 import jwt from "jsonwebtoken";
 
 export const checkToken = (req, res, next) => {
