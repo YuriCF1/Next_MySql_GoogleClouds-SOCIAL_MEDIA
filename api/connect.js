465fc1dd8d78699471c28621e99aec4aa3024a14
Yuri Cruz <yuricruzf@gmail.com>
2024-06-21 04:02:30 -0300

    Debugging

--- api/connect.js
+++ api/connect.js
@@ -4,6 +4,14 @@ import dotenv from "dotenv";
 // dotenv.config({ path: "./.env" });
 dotenv.config();
 
+const requiredEnvVars = ['DB_HOST', 'DB_USER', 'DB_PASS', 'DB'];
+for (const varName of requiredEnvVars) {
+  if (!process.env[varName]) {
+    console.error(`Erro: A variável de ambiente ${varName} não está definida.`);
+    process.exit(1);
+  }
+}
+
 export const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
