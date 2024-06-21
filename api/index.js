cd5626b2047f7d4fa8d0b402c00053203e52c4c4
Yuri Cruz <yuricruzf@gmail.com>
2024-06-21 11:47:40 -0300

    Changing cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -7,7 +6,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
 import postRouter from "./routes/post.js";
-import searcHRouter from "./routes/search.js";
+import searchRouter from "./routes/search.js";
 import uploadRouter from "./routes/upload.js";
 import commentRouter from "./routes/comments.js";
 import likesRouter from "./routes/likes.js";
@@ -15,26 +14,14 @@ import friendshipRouter from "./routes/friendship.js";
 
 const app = express();
 
-//Fazendo o app entender o formato json das respostas vindas do controller
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://next-my-sql-sigma.vercel.app",
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    // Permite pedidos sem origem (como aplicações móveis ou servidores internos)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, //Esta opção indica se a requisição pode incluir cookies, autorizações HTTP ou certificados TLS. Quando configurada como true, o servidor aceita e envia cookies com as requisições.
+  origin: [
+    "http://localhost:3000",
+    "https://your-vercel-app-url.vercel.app", // Adicione aqui o domínio da sua aplicação na Vercel
+  ],
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
     "Content-Type",
@@ -43,16 +30,10 @@ const corsOptions = {
   ],
 };
 
-/*Esta opção define quais cabeçalhos HTTP podem ser usados quando a solicitação é feita. No caso, os cabeçalhos permitidos são:
-
-Content-Type: Define o tipo de mídia do recurso.
-Authorization: Usado para enviar informações de autenticação.
-Access-Control-Allow-Credentials: Indica se a resposta pode ser exposta ao navegador e se o navegador deve enviar credenciais como cookies junto com a requisição.*/
-
-//Middleware
-app.use(bodyParser.urlencoded({ extended: false })); //Recebendo os dados pelo método x-www-form-urlencoded do postman
-app.use(cors(corsOptions)); //crediantals: true = o servidor pode enviar e receber cookies e cabeçalhos de autenticação.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 app.use(cookieParser());
+
 app.use("/api/users/", userRouter);
 app.use("/api/auth/", authRouter);
 app.use("/api/post/", postRouter);
@@ -60,9 +41,9 @@ app.use("/api/upload/", uploadRouter);
 app.use("/api/comments/", commentRouter);
 app.use("/api/likes/", likesRouter);
 app.use("/api/friendship/", friendshipRouter);
-app.use("/api/search/", searcHRouter);
+app.use("/api/search/", searchRouter);
 
-const portS = process.env.PORTS;
+const portS = process.env.PORTS || 8001;
 app.listen(portS, () => {
-  console.log("Servidor rodando para a porta 8001");
+  console.log(`Servidor rodando na porta ${portS}`);
 });
