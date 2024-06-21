eeef104d7716c72cf382b351a8dffef43626ed60
Yuri Cruz <yuricruzf@gmail.com>
2024-06-21 03:18:26 -0300

    Updating cors

--- api/index.js
+++ api/index.js
@@ -7,20 +7,33 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
 import postRouter from "./routes/post.js";
-import searcHRouter from "./routes/search.js"
+import searcHRouter from "./routes/search.js";
 import uploadRouter from "./routes/upload.js";
 import commentRouter from "./routes/comments.js";
 import likesRouter from "./routes/likes.js";
 import friendshipRouter from "./routes/friendship.js";
-
 
 const app = express();
 
 //Fazendo o app entender o formato json das respostas vindas do controller
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://next-my-sql-sigma.vercel.app",
+];
+
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: (origin, callback) => {
+    // Permite pedidos sem origem (como aplicações móveis ou servidores internos)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true, //Esta opção indica se a requisição pode incluir cookies, autorizações HTTP ou certificados TLS. Quando configurada como true, o servidor aceita e envia cookies com as requisições.
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
@@ -47,7 +60,7 @@ app.use("/api/upload/", uploadRouter);
 app.use("/api/comments/", commentRouter);
 app.use("/api/likes/", likesRouter);
 app.use("/api/friendship/", friendshipRouter);
-app.use("/api/search/", searcHRouter)
+app.use("/api/search/", searcHRouter);
 
 const portS = process.env.PORTS;
 app.listen(portS, () => {
