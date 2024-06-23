21501b2c2c01353401d481d1293e83ddfeec2f97
Yuri Cruz <yuricruzf@gmail.com>
2024-06-23 04:20:55 -0300

    Fixing bug

--- client/social-media/src/components/TestUser.tsx
+++ client/social-media/src/components/TestUser.tsx
@@ -2,8 +2,8 @@ const TestUser = () => {
     return (
         <div className="bg-green-200 flex flex-col rounded-md p-5 mt-3">
             <div className="text-2xs mb-4">
-                <p>- If you don't want to create an account, just test the app {':)'}</p>
-                <p>- Caso não queiras criar uma conta, apenas testar o aplicativo {':)'}</p>
+                <p>- If you don't want to create an account, just test the app {`:)`}</p>
+                <p>- Caso não queiras criar uma conta, apenas testar o aplicativo {`:)`}</p>
             </div>
             <div className="flex flex-col">
                 <h4 className="text-center">Test User</h4>
