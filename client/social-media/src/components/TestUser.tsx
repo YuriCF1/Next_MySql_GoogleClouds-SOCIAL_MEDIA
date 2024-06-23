0035abdd167fa81eeb14ca523091fccedae7c5e6
Yuri Cruz <yuricruzf@gmail.com>
2024-06-23 04:16:56 -0300

    Fixing user test

--- client/social-media/src/components/TestUser.tsx
+++ client/social-media/src/components/TestUser.tsx
@@ -7,7 +7,7 @@ const TestUser = () => {
             </div>
             <div className="flex flex-col">
                 <h4 className="text-center">Test User</h4>
-                <span>Email: text@user.com</span>
+                <span>Email: user@test.com</span>
                 <span>Password: 123456</span>
             </div>
         </div>
