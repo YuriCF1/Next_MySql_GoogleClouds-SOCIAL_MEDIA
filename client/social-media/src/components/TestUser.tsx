ce48a748984fbfbfe1ea48930dc546e2154abd28
Yuri Cruz <yuricruzf@gmail.com>
2024-07-06 16:15:37 -0300

    Putting server back on

--- client/social-media/src/components/TestUser.tsx
+++ client/social-media/src/components/TestUser.tsx
@@ -1,13 +1,14 @@
 const TestUser = () => {
     return (
         <div className="bg-blue-200 flex flex-col rounded-md p-5 mt-3">
-        {/* <div className="bg-red-200 flex flex-col rounded-md p-5 mt-3"> */}
-            <p>- AWS started to send me bills because of the public IP that i was using in the RDS. So i changed to Google Cloud but they stoped to offer Cloud SQL in Free Tier. So unfortunally i had to turn off the server...
-                But I{`'`}m searching other free solution for it!</p>
-            <p>- A AWS começou a me cobrar em dólares pelo IP público que eu estava usando para conectar com a RDS... Daí migrei para a Google Cloud, mas eles pararam de oferecer o Cloud SQL no nível grauito... então infelizmente tive que fechar o servidor.
-                Porém estou buscando uma maneira gratuíta para hospedá-lo!
-            </p>
-            {/* <div className="text-2xs mb-4">
+            {/* <div className="bg-red-200 flex flex-col rounded-md p-5 mt-3">
+                <p>- AWS started to send me bills because of the public IP that i was using in the RDS. So i changed to Google Cloud but they stoped to offer Cloud SQL in Free Tier. So unfortunally i had to turn off the server...
+                    But I{`'`}m searching other free solution for it!</p>
+                <p>- A AWS começou a me cobrar em dólares pelo IP público que eu estava usando para conectar com a RDS... Daí migrei para a Google Cloud, mas eles pararam de oferecer o Cloud SQL no nível grauito... então infelizmente tive que fechar o servidor.
+                    Porém estou buscando uma maneira gratuíta para hospedá-lo!
+                </p>
+            </div> */}
+            <div className="text-2xs mb-4">
                 <p>- If you don{`'`}t want to create an account, just test the app {`:)`}</p>
                 <p>- Caso não queiras criar uma conta, apenas testar o aplicativo {`:)`}</p>
             </div>
@@ -15,7 +16,7 @@ const TestUser = () => {
                 <h4 className="text-center">Test User</h4>
                 <span>Email: test@email.com</span>
                 <span>Password: iwouldlovetohelpyourteam</span>
-            </div> */}
+            </div>
         </div>
     )
 }
