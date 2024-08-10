bc711cd8e9bfc4eed1da001ef62a1a529240e23b
Yuri Cruz <yuricruzf@gmail.com>
2024-08-10 02:08:51 -0300

    Shutting down server

--- client/social-media/src/components/TestUser.tsx
+++ client/social-media/src/components/TestUser.tsx
@@ -1,6 +1,7 @@
 const TestUser = () => {
     return (
-        <div className="bg-blue-200 flex flex-col rounded-md p-5 mt-3">
+        // <div className="bg-blue-200 flex flex-col rounded-md p-5 mt-3">
+        <div className="bg-red-200 flex flex-col rounded-md p-5 mt-3">
             {/* <div className="bg-red-200 flex flex-col rounded-md p-5 mt-3">
                 <p>- AWS started to send me bills because of the public IP that i was using in the RDS. So i changed to Google Cloud but they stoped to offer Cloud SQL in Free Tier. So unfortunally i had to turn off the server...
                     But I{`'`}m searching other free solution for it!</p>
@@ -9,14 +10,16 @@ const TestUser = () => {
                 </p>
             </div> */}
             <div className="text-2xs mb-4">
-                <p>- If you don{`'`}t want to create an account, just test the app {`:)`}</p>
-                <p>- Caso não queiras criar uma conta, apenas testar o aplicativo {`:)`}</p>
+                <p className="mt-2 text-sm"><b>ENGLISH:</b> This backend server, previously hosted on Google Cloud's Compute Engine, has been taken offline as of October 2024 to avoid unnecessary costs. However, the backend code and implementation details remain available for review. Feel free to explore how I built the connections and structured the backend on my GitHub repository <a href="https://github.com/YuriCF1/Next_MySql_GoogleClouds-SOCIAL_MEDIA/tree/main/api" className="text-blue-700">here</a>.</p>
+                <p className="mt-2 text-sm"><b>PORTUGUÊS:</b> Este servidor backend, anteriormente hospedado no Compute Engine da Google Cloud, foi desativado em outubro de 2024 para evitar custos desnecessários. No entanto, o código do backend e os detalhes da implementação continuam disponíveis para consulta. Fique à vontade para conferir como fiz as conexões e estruturei o backend no meu repositório do GitHub <a href="https://github.com/YuriCF1/Next_MySql_GoogleClouds-SOCIAL_MEDIA/tree/main/api" className="text-blue-700">aqui</a>.</p>
+                {/* <p>- If you don{`'`}t want to create an account, just test the app {`:)`}</p>
+                <p>- Caso não queiras criar uma conta, apenas testar o aplicativo {`:)`}</p> */}
             </div>
-            <div className="flex flex-col">
+            {/* <div className="flex flex-col">
                 <h4 className="text-center">Test User</h4>
                 <span>Email: test@email.com</span>
                 <span>Password: iwouldlovetohelpyourteam</span>
-            </div>
+            </div> */}
         </div>
     )
 }
